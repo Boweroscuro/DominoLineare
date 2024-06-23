efca3f67a198a44405602cfc33a0c80188be08fe
Francesco Gerlin <117569188+Boweroscuro@users.noreply.github.com>
2024-06-23 17:29:21 +0200

    Delete DominoLineare directory

--- DominoLineare/GameManagement.h
+++
@@ -1,8 +0,0 @@
-#include"GameManagement.c"
-
-void printTessere();
-nodo* createNodo(int x, int y);
-void prepend(nodo** l, int x, int y);
-void append(nodo** l, int x, int y);
-void PrintingGame(nodo* g, nodo* T);
-void freeList(nodo* l);
